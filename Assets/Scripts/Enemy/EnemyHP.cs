73155bd0f84f7f18345f7b5aeac5fa2cc9454aea
HyeonxJ <b.bhj0817@gmail.com>
2025-06-02 17:27:55 +0900

    EnemyAI
    적 구현
    - 자동 타겟팅 (AI 동료 or 플레이어)
    - range 2f 안에 들어오면 공격

---
+++ Assets/Scripts/Enemy/EnemyHP.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int maxHealth = 50;
+    private int currentHealth;
+
+    Animator enemyAnimator;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        enemyAnimator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        enemyAnimator.SetTrigger("Die");
+        Debug.Log("Player died!");
+        Destroy(gameObject, 3f);
+    }
+}
